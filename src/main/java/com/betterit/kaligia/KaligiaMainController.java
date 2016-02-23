190692b650a2881f06dc5b8aaf4a00bbb54ec70f
nayar <lnayar@gmail.com>
2016-02-23 15:49:51 -0500

    controller for main page

---
+++ src/main/java/com/betterit/kaligia/KaligiaMainController.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package com.betterit.kaligia;
+
+import java.util.List;
+import java.util.logging.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+/**
+ * @author nayar
+ *
+ */
+
+
+@Controller
+public class KaligiaMainController {
+
+	Logger log = Logger.getLogger(KaligiaMainController.class.getName());
+
+	@Autowired
+	private TestProcedureService procedureServiceObject;
+	
+	@RequestMapping(value="/", method=RequestMethod.GET)
+    public String mainForm(Model model) {
+		
+		/** TO-DO get all procedures from database */
+		List<TestProcedure> procedureList = procedureServiceObject.getAll();
+		
+		model.addAttribute("ProcedureList", procedureList);
+		
+		return ("KaligiaMainApp");
+	}
+}
