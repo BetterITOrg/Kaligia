a60df794a53fa54dd787f0f65a836cc291d33611
nayar <lnayar@gmail.com>
2016-02-23 21:59:23 -0500

    defined for segments

---
+++ src/main/java/com/betterit/kaligia/segmentParams.java
@@ -0,0 +1,63 @@
+/**
+ * 
+ */
+package com.betterit.kaligia;
+
+/**
+ * @author nayar
+ *
+ */
+public class segmentParams {
+
+	private String integrationTime;
+	private String scan2Average;
+	private String boxCarWidth;
+	private String electricDark;
+	private String nonLinearCorrect;
+	private String delay;
+	private String power;
+	public String getIntegrationTime() {
+		return integrationTime;
+	}
+	public void setIntegrationTime(String integrationTime) {
+		this.integrationTime = integrationTime;
+	}
+	public String getScan2Average() {
+		return scan2Average;
+	}
+	public void setScan2Average(String scan2Average) {
+		this.scan2Average = scan2Average;
+	}
+	public String getBoxCarWidth() {
+		return boxCarWidth;
+	}
+	public void setBoxCarWidth(String boxCarWidth) {
+		this.boxCarWidth = boxCarWidth;
+	}
+	public String getElectricDark() {
+		return electricDark;
+	}
+	public void setElectricDark(String electricDark) {
+		this.electricDark = electricDark;
+	}
+	public String getNonLinearCorrect() {
+		return nonLinearCorrect;
+	}
+	public void setNonLinearCorrect(String nonLinearCorrect) {
+		this.nonLinearCorrect = nonLinearCorrect;
+	}
+	public String getDelay() {
+		return delay;
+	}
+	public void setDelay(String delay) {
+		this.delay = delay;
+	}
+	public String getPower() {
+		return power;
+	}
+	public void setPower(String power) {
+		this.power = power;
+	}
+	
+	
+}
