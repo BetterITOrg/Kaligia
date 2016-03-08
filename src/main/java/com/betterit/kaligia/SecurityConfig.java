79ac398f38764d4912ff50f583fedbf8ecf5842a
kjohar <kjohar@yahoo.com>
2016-03-08 13:06:01 -0500

    Enabled Spring Security

---
+++ src/main/java/com/betterit/kaligia/SecurityConfig.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package com.betterit.kaligia;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author V135012
+ *
+ */
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.headers().frameOptions().sameOrigin();
+		http.authorizeRequests().anyRequest().permitAll();
+		http.httpBasic();
+		http.csrf().disable();
+	}
+
+		
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+            .inMemoryAuthentication()
+                .withUser("user").password("password").roles("USER")
+                .and()
+                .withUser("admin").password("admin").roles("ADMIN");
+    }
+    
+}
