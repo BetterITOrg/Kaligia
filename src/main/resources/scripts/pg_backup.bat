2af5d32b47b6b82d9a6eb5fdaaca81273d0f9153
kjohar <kjohar@yahoo.com>
2016-04-06 09:10:57 -0400

    DB backup script

---
+++ src/main/resources/scripts/pg_backup.bat
@@ -0,0 +1,36 @@
+@echo off
+setlocal enabledelayedexpansion
+
+for /f "tokens=1-4 delims=/ " %%i in ("%date%") do (
+     set dow=%%i
+     set month=%%j
+     set day=%%k
+     set year=%%l
+   )
+
+set datestr=%month%_%day%_%year%
+echo datestr is %datestr%
+
+set epn=KBS01
+echo epn is %epn%
+    
+set BACKUP_FILE=%epn%_%datestr%.backup
+
+if not exist "%BACKUP_FILE%" goto SkipRename
+
+for %%f in ("%BACKUP_FILE%") do (
+    set NewName=%%~nf.old%%~xf
+
+    if exist "!NewName!" del /F "!NewName!"
+ 
+    echo Renaming "%BACKUP_FILE%" to "!NewName!" ...
+    ren "%BACKUP_FILE%" "!NewName!"
+)
+:SkipRename
+
+   echo backup file name is %BACKUP_FILE%
+   echo on
+   "C:\Program Files\PostgreSQL\9.5\bin\pg_dump.exe" -h localhost -p 5432 -U postgres -O -F p -n kaligia -d kaligia -f %BACKUP_FILE% 
+	
+::"C:\Program Files\PostgreSQL\9.5\bin\psql.exe" -h localhost -p 5432 -U postgres -d kaligia -f %BACKUP_FILE%
+::echo ftp -s:FileName once backup file is generated.
