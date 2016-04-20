783ea1524157246121c5750143f5ab7c824c727d
Nigel Struble <nstruble@kaligiabiosciences.com>
2016-04-20 14:42:49 -0400

    fixed conflicting class names

--- src/main/java/com/betterit/kaligia/analytics/analysis menu/postgresdb.cpp
+++ src/main/java/com/betterit/kaligia/analytics/analysis menu/postgresdb.cpp
@@ -8,53 +8,7 @@
 
 using namespace std;
 using namespace pqxx;
-class Wavenumber{
- public:
- int wavenum;
- double photon_count;
-};
-class Repeat{
- public:
- vector<Wavenumber> wavenumbers;
-};
-class Sample{
- public:
- vector<Repeat> repeats;
- Repeat avg_repeat(){
-  Repeat avg;
-  avg.wavenumbers.resize(repeats[0].wavenumbers.size());
-  for(int i = 0; i < (int)avg.wavenumbers.size(); ++i){
-   avg.wavenumbers[i].photon_count = 0;
-   avg.wavenumbers[i].wavenum = repeats[0].wavenumbers[i].wavenum;
-   for(int j = 0; j < (int)repeats.size(); ++j){
-    avg.wavenumbers[i].photon_count += repeats[j].wavenumbers[i].photon_count*1.0 / repeats.size();
-   }
-  }
-  return avg;
- }
- double bgl;
-};
-class Test{
- public:
- vector<Sample> samples;
- Sample average_sample(){
-  Sample avg;
-  avg.repeats.resize(samples[0].repeats.size());
-  for(int i = 0; i < (int)avg.repeats.size(); ++i){
-   for(int j = 0; j < (int)samples[0].repeats[i].wavenumbers.size(); ++j){
-    double avg_photon_count = 0;
-    for(int k = 0; k < (int)samples.size(); ++k){
-     avg_photon_count += samples[k].repeats[i].wavenumbers[j].photon_count*1.0/samples.size();
-    }
-    Wavenumber wavenum;
-    wavenum.photon_count = avg_photon_count;
-    wavenum.wavenum = samples[0].repeats[i].wavenumbers[j].wavenum;
-    avg.repeats[i].wavenumbers.push_back(wavenum);
-   }
-  }
-  return avg;
- }
-};
+
 void postgresdb::load_db(settings &s){
   try{
    connection C("dbname=kaligia user=postgres password=postgres hostaddr=192.168.1.135 port=5432");
@@ -94,7 +48,7 @@ void postgresdb::load_db(settings &s){
     result R(N.exec(oss.str().c_str()));
     oss.str("");
     cout << "id " << run_ids[i] << endl;
-    Sample sample;
+    db_sample sample;
     
     for(int j = 0; j < (int)R.size(); ++j){
      oss << "select wavelength, photon_count from kaligia.flremovedlog where run_segment_id = " << R[j][0] << " order by wavelength;";
@@ -112,11 +66,9 @@ void postgresdb::load_db(settings &s){
      
      sample.repeats.push_back(repeat);
     }
-    oss << "select * from kaligia.runorder where run_id = " << run_ids[i] << ";";
-    cout << oss.str() << endl;
+    oss << "select run_notes, result_notes from kaligia.runorder where run_id = " << run_ids[i] << ";";
     result BG_Result(N.exec(oss.str().c_str()));
     oss.str("");
-    clog << "executed" << endl;
     sample.bgl = BG_Result[0][0].as<double>()/2 + BG_Result[0][1].as<double>()/2;
     test.samples.push_back(sample);
    }
@@ -130,7 +82,7 @@ void postgresdb::load_db(settings &s){
    for(int i = 0; i < (int)test.samples.size(); ++i){
     ofs << test.samples[i].bgl;
     Repeat avg = test.samples[i].avg_repeat();
-    for(int j = 0; j < (int)avg.wavenumbers.size(); ++i){
+    for(int j = 0; j < (int)avg.wavenumbers.size(); ++j){
      ofs << "," << avg.wavenumbers[j].photon_count;
     }
     ofs << endl;
--- src/main/java/com/betterit/kaligia/analytics/analysis menu/postgresdb.h
+++ src/main/java/com/betterit/kaligia/analytics/analysis menu/postgresdb.h
@@ -1,6 +1,54 @@
 #ifndef __POSTGRESDB_H__
 #define __POSTGRESDB_H__
 #include "settings.h"
+#include <vector>
+class Wavenumber{
+ public:
+ int wavenum;
+ double photon_count;
+};
+class Repeat{
+ public:
+ std::vector<Wavenumber> wavenumbers;
+};
+class db_sample{
+ public:
+ std::vector<Repeat> repeats;
+ Repeat avg_repeat(){
+  Repeat avg;
+  avg.wavenumbers.resize(repeats[0].wavenumbers.size());
+  for(int i = 0; i < (int)avg.wavenumbers.size(); ++i){
+   avg.wavenumbers[i].photon_count = 0;
+   avg.wavenumbers[i].wavenum = repeats[0].wavenumbers[i].wavenum;
+   for(int j = 0; j < (int)repeats.size(); ++j){
+    avg.wavenumbers[i].photon_count += repeats[j].wavenumbers[i].photon_count*1.0 / repeats.size();
+   }
+  }
+  return avg;
+ }
+ double bgl;
+};
+class Test{
+ public:
+ std::vector<db_sample> samples;
+ /*Sample average_sample(){
+  Sample avg;
+  avg.repeats.resize(samples[0].repeats.size());
+  for(int i = 0; i < (int)avg.repeats.size(); ++i){
+   for(int j = 0; j < (int)samples[0].repeats[i].wavenumbers.size(); ++j){
+    double avg_photon_count = 0;
+    for(int k = 0; k < (int)samples.size(); ++k){
+     avg_photon_count += samples[k].repeats[i].wavenumbers[j].photon_count*1.0/samples.size();
+    }
+    Wavenumber wavenum;
+    wavenum.photon_count = avg_photon_count;
+    wavenum.wavenum = samples[0].repeats[i].wavenumbers[j].wavenum;
+    avg.repeats[i].wavenumbers.push_back(wavenum);
+   }
+  }
+  return avg;
+ }*/
+};
 class postgresdb{
  public:
   static void load_db(settings &s);
