02e7b4e4f3d2e4bc58c84d7b8869d42d48dfdac5
nayar <lnayar@gmail.com>
2016-05-16 15:00:20 -0400

    chnages to handle logout

--- src/main/java/com/betterit/kaligia/controller/LoginController.java
+++ src/main/java/com/betterit/kaligia/controller/LoginController.java
@@ -6,7 +6,11 @@ package com.betterit.kaligia.controller;
 import java.util.logging.Logger;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -29,4 +33,13 @@ public class LoginController {
         log.info("login: " + req);
         return "login";
     }
+	
+	@RequestMapping(value="/logout", method = RequestMethod.GET)
+	public String logoutPage (HttpServletRequest request, HttpServletResponse response) {
+	    Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+	    if (auth != null){    
+	        new SecurityContextLogoutHandler().logout(request, response, auth);
+	    }
+	    return "redirect:/login?logout";//You can redirect wherever you want, but generally it's a good practice to show login screen again.
+	}
 }
