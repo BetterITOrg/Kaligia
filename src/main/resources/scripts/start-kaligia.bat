ba1dd2a80c4ca439efb282f3abd38fdb21a0cdbb
Huzefa Ghadiyali <huzefasg@gmail.com>
2016-05-19 10:14:56 -0400

    Added timestamp to Console file

--- src/main/resources/scripts/start-kaligia.bat
+++ src/main/resources/scripts/start-kaligia.bat
@@ -1,19 +1,23 @@
 @ECHO ON
 
 :: Set Java variable
-setx JAVA_HOME "%HOMEDRIVE%\program files\Java\jdk1.8.0_31" > nul 2>&1
+setx JAVA_HOME "c:\Progra~1\Java\jdk1.8.0_73" /m
 
 :: Set Kaligia home variable
-setx KALIGIA_HOME "%HOMEDRIVE%\Users\v135012\Documents\VentureE\Kaligia\App"  > nul 2>&1
+setx KALIGIA_HOME "c:\Kaligia" /m
 
 :: Change working directory
 :: cd $KALIGIA_HOME
 
 :: Spring Parameters
-set SPRING_CONFIG_LOC=file:C:/Users/v135012/Documents/VentureE/Kaligia/config/application.yml
+set SPRING_CONFIG_LOC=file:C:/Kaligia/config/application.yml
 set SPRING_ACTIVE_PROFILE=local
 
-:: Start App
-start /b "kaligia" "%JAVA_HOME%\bin\java.exe" -jar %KALIGIA_HOME%\bin\kaligia-1.0.jar -Dspring.active.profile=%SPRING_ACTIVE_PROFILE% -Dspring.config.location=%SPRING_CONFIG_LOC%  > %KALIGIA_HOME%\logs\console.out 2>&1
+:: Time Stamp
+set CurrentDate=%date:~-4,4%%date:~-7,2%%date:~-10,2%
+set CurrentTime=%time:~-11,2%%time:~-8,2%
 
-exit
+:: Start App
+start /b "kaligia" "%JAVA_HOME%\bin\java" -jar C:\Kaligia\bin\kaligia-1.0.jar -Dspring.active.profile=%SPRING_ACTIVE_PROFILE% -Dspring.config.location=%SPRING_CONFIG_LOC%  > %KALIGIA_HOME%\logs\console_%CurrentDate%_%CurrentTime%.out 2>&1
+
+::exit
